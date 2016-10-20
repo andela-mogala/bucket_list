781863ffd016ffe4f6421783b77f3741f5e8bee1
Michael Ogala <michael.ogala@andela.com>
2016-10-20 14:59:22 +0100

    finish test for create action of users controller

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -1,26 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::UsersController, type: :controller do
+  describe 'GET #show' do
+  end
+
   describe 'POST #create' do
     let(:user_attributes) { FactoryGirl.attributes_for(:user) }
+    let!(:initial_user_count) { User.count }
 
     context 'with valid parameters' do
-      let!(:initial_user_count) { User.count }
       before { post :create, user: user_attributes, format: :json }
 
-      it 'should create a user' do
+      it 'creates a user' do
         expect(User.count).to eq initial_user_count + 1
       end
 
-      it 'should render a json response containing user details' do
+      it 'renders a json response containing user details' do
         user_response = JSON.parse(response.body, symbolize_names: true)
         expect(user_response[:first_name]).to eq user_attributes[:first_name]
         expect(user_response[:last_name]).to eq user_attributes[:last_name]
         expect(user_response[:email]).to eq user_attributes[:email]
       end
 
-      it 'should have a response code indicating success' do
+      it 'has a response code indicating success' do
         expect(response.status).to eq 201
+      end
+    end
+
+    context 'with invalid parameters' do
+      before do
+        user_attributes[:email] = nil
+        post :create, user: user_attributes, format: :json
+      end
+
+      it 'does not create a user' do
+        expect(User.count).to eq initial_user_count
+      end
+
+      it 'renders a json response containing errors' do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:errors]).to be_present
+      end
+
+      it 'has a response code indicating failure' do
+        expect(response.status).to eq 401
       end
     end
   end
