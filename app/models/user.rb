bbb2dfc679462ce1e85ea87e826d8fe6a2868f8d
Michael Ogala <michael.ogala@andela.com>
2016-10-20 12:11:00 +0100

    add user model spec with validations

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ActiveRecord::Base
+  has_secure_password
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true,
+                    uniqueness: true
+  validates :password, length: { minimum: 8 },
+                        confirmation: true
+  validates :password_confirmation, presence: true
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount SabisuRails::Engine => "/sabisu_rails"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
