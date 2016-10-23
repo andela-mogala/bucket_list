f89114615bc0ff7a1a3e398b6309a0ceedb88404
Michael Ogala <michael.ogala@andela.com>
2016-10-23 02:53:04 +0100

    add login action in auth controller

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,2 +1,23 @@
 class Api::V1::AuthController < ApplicationController
+  respond_to :json
+
+  def login
+    user = User.find_by(email: params[:email])
+    return render json: { errors: ['Invalid email/password'] } unless
+      user.authenticate(params[:password])
+    render json: payload(user)
+  end
+
+  def logout
+  end
+
+  private
+
+  def payload(user)
+    return nil unless user && user.id
+    {
+      auth_token: JsonWebToken.encode({user_id: user.id}),
+      user: { id: user.id, email: user.email }
+    }
+  end
 end
--- spec/controllers/api/v1/auth_controller_spec.rb
+++ spec/controllers/api/v1/auth_controller_spec.rb
@@ -2,4 +2,21 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::AuthController, type: :controller do
 
+  describe 'POST #login' do
+    let(:user) { create :user }
+
+    context 'with valid params' do
+      before { post :login, email: user.email, password: user.password }
+
+      it 'returns a response containing an authentication token' do
+        expect(json_response[:auth_token]).to be_present
+      end
+    end
+
+    context 'with invalid params' do
+    end
+  end
+
+  describe 'GET #logout' do
+  end
 end
