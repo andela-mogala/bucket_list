86d0befd726793b8f8b07bd5dcb35cec5967cbce
Michael Ogala <michael.ogala@andela.com>
2016-10-26 12:00:12 +0100

    setup UI for users to sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,8 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
-    return render 'new' unless user.save
+    @user = User.new(user_params)
+    return render 'new' unless @user.save
     redirect_to action: 'show'
   end
 
