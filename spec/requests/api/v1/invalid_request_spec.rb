532b29a044d52698772cd8cdf332c1af9ff6dbbd
Michael Ogala <michael.ogala@andela.com>
2016-10-26 18:16:25 +0100

    test invalid routes

---
+++ spec/requests/api/v1/invalid_request_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe 'Invalid routes', type: :request do
+  let(:user) { create :user }
+  let(:header) { { authorization: user.auth_token } }
+
+  describe 'GET /some_route' do
+    context 'when route doesn\'t exist' do
+      before { get '/people', {}, header }
+
+      it 'returns an error json' do
+        expect(json_response[:error]).to be_present
+      end
+
+      it 'returns status indicating resource is not found' do
+        expect(response.status).to eq 400
+      end
+    end
+  end
+end
