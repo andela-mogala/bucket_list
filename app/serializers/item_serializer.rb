01077901cf4a66da1534ed39605f86af0dcd8ea3
Michael Ogala <michael.ogala@andela.com>
2016-10-30 21:41:39 +0100

    remove updated_at and created_at from item serializer

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,5 +1,5 @@
 class ItemSerializer < ActiveModel::Serializer
-  attributes :id, :name, :done, :created_at, :updated_at
+  attributes :id, :name, :done
   attribute :created_at, key: :date_created
   attribute :updated_at, key: :date_updated
 end
